f7e58a11cf2b7416716ab019afa699f9e72a3476
Prashant Jadon <prashantjadon@Prashants-MacBook-Air.local>
2024-09-13 10:51:22 +0530

    Connected to DB

--- connection.js
+++ connection.js
@@ -1,2 +1,15 @@
 //todo
 const mongoose = require('mongoose')
+
+async function connectTODb(uri) {
+   try {
+    mongoose.connect(uri);
+    console.log("Connceted to DB");
+   } catch (error) {
+    console.log(error)
+   }
+}
+
+module.exports = {
+    connectTODb
+}
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const axios = require('axios');
 const express = require('express');
 const app = express();
 const {router} = require('./routes/subdRoutes')
+const {connectTODb} = require('./connection')
 
 app.use(express.json()); 
+
+connectTODb('mongodb://127.0.0.1:27017');
 
 app.use('/subdomains',router);
 
